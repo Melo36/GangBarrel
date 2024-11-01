c52107d81cf2b9e92ffbd1d3063c9d769f110486
Melo36 <melihyilmazrdg@gmail.com>
2024-11-01 22:32:12 +0100

    Made tiles fit the grid and added dummy barrel

--- GangBarrel/Assets/Scripts/TileDetection.cs
+++ GangBarrel/Assets/Scripts/TileDetection.cs
@@ -1,15 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class TileDetection : MonoBehaviour
 {
     private Tilemap tileMap;
-    
     private Vector3Int location;
-
     public Camera mainCamera;
+
     void Start()
     {
         tileMap = GetComponentInChildren<Tilemap>();
@@ -17,12 +17,53 @@ public class TileDetection : MonoBehaviour
 
     void Update()
     {
+        bool barrel = false;
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, mainCamera.transform.position.z);
-            Vector3 mp = mainCamera.ScreenToWorldPoint(mousePosition);
-            location = tileMap.WorldToCell(mp);
-            Debug.Log("Position x: " + location.x + " y: " + location.y + " z: " + location.z);
+            // Cast a ray from the camera to the point clicked on the screen
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+            Vector3 direction = (Input.mousePosition - mainCamera.transform.position).normalized;
+            if (Physics.Raycast(mainCamera.transform.position, direction, out RaycastHit hit,
+                    Mathf.Infinity))
+            {
+                Debug.Log("Hit something");
+                if (hit.transform.gameObject.CompareTag("Barrel"))
+                {
+                    barrel = true;
+                }
+            }
+
+            // Set a plane at y=0 to intersect with the ray (assuming the tilemap is on the XZ plane at y=0)
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+            
+            // Check if the ray hits the XZ plane
+            float distance;
+            if (plane.Raycast(ray, out distance))
+            {
+                // Get the point on the XZ plane where the ray hit
+                Vector3 worldPosition = ray.GetPoint(distance);
+
+                // Convert the world position to a cell position in the tilemap
+                location = tileMap.WorldToCell(worldPosition);
+
+                // Check if a tile exists at this cell location
+                TileBase tileBase = tileMap.GetTile(location);
+                TileData data = new TileData();
+                tileBase.GetTileData(location,tileMap,ref data);
+                if (barrel)
+                {
+                    Debug.Log("Barrel detected at Position x: " + location.x + " y: " + location.y + " z: " + location.z);
+                }
+                else if (tileMap.GetTile(location) != null)
+                {
+                    Debug.Log("Tile detected at Position x: " + location.x + " y: " + location.y + " z: " + location.z);
+                }
+                else
+                {
+                    Debug.Log("No tile at Position x: " + location.x + " y: " + location.y + " z: " + location.z);
+                }
+            }
         }
     }
 }
