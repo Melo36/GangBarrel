cc929a6df9fe10f15ac8173ca401c04ec63196cc
Melo36 <melihyilmazrdg@gmail.com>
2024-11-24 15:15:27 +0100

    Fuck CustomTiles, all my homies hate CustomTiles (Added chain explosion for barrels)

--- GangBarrel/Assets/Scripts/BarrelExplosionController.cs
+++ GangBarrel/Assets/Scripts/BarrelExplosionController.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class BarrelExplosionController : MonoBehaviour
 {
     public ParticleSystem explosion;
+    public GameObject explosionRadius;
     private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.CompareTag("Bullet"))
+        if (other.gameObject.CompareTag("Bullet") || other.gameObject.CompareTag("ExplosionTrigger"))
         {
             Instantiate(explosion, transform.position, Quaternion.identity);
+            Instantiate(explosionRadius, transform.position, Quaternion.identity);
             Destroy(gameObject);
             Destroy(other.gameObject);
         }
